543465615b540cfd2db6c3248359e99909e51d37
star-creator <mail.starcreator@gmail.com>
2019-07-07 17:20:08 +0100

    completed header style and navigation script

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,9 +1,8 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-browserSync = require('browser-sync').create();
+var gulp = require("gulp"),
+  watch = require("gulp-watch"),
+  browserSync = require("browser-sync").create();
 
-gulp.task('watch', function() {
-
+gulp.task("watch", function() {
   browserSync.init({
     notify: false,
     server: {
@@ -11,17 +10,23 @@ gulp.task('watch', function() {
     }
   });
 
-  watch('./app/index.html', function() {
+  watch("./app/index.html", function() {
     browserSync.reload();
   });
 
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('cssInject');
+  watch("./app/assets/styles/**/*.css", function() {
+    gulp.start("cssInject");
   });
 
+  watch("./app/assets/scripts/**/*.js", function() {
+    gulp.start("scriptsRefresh");
+  });
 });
 
-gulp.task('cssInject', ['styles'], function() {
-  return gulp.src('./app/temp/styles/styles.css')
-    .pipe(browserSync.stream());
+gulp.task("cssInject", ["styles"], function() {
+  return gulp.src("./app/temp/styles/styles.css").pipe(browserSync.stream());
+});
+
+gulp.task("scriptsRefresh", ["scripts"], function() {
+  browserSync.reload();
 });
